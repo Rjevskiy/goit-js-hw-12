f989683b0eb1a1d51108b1e21df08de13286177c
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-11-10 15:13:50 +0300

    ок

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function fetchImages(query, page = 1) {
     const API_KEY = '46859112-8db04929d193e6e9d044d366e';
     const BASE_URL = 'https://pixabay.com/api/';
-    const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=100&page=${page}`;
+    const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`;
 
     const response = await axios.get(url);
     if (response.status !== 200) {
